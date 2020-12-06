ccf662b7433ab14e168b26a9bfbbb9026b964e98
Baranemlek <75400163+Baranemlek@users.noreply.github.com>
2020-12-06 04:37:27 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,18 +19,15 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Hi {}, my name is {}! If you have any questions on how to use me, read /help - and then head to @MarieSupport.
+Komutlar için /help
 
-I'm a group manager bot built in python3, using the python-telegram-bot library, and am fully opensource; \
-you can find what makes me tick [here](github.com/PaulSonOfLars/tgbot)!
+Bu bot grub yönetim botudur
 
-Feel free to submit pull requests on github, or to contact my support group, @MarieSupport, with any bugs, questions \
-or feature requests you might have :)
-I also have a news channel, @MarieNews for announcements on new features, downtime, etc.
+Yetki vermeniz gerekir aksi halde botu sürekli başlatmanız gerekir yetkisi yokken mesajlara iletişimi yoktur
 
-You can find the list of available commands with /help.
+Botun sahibi yani ben @Baran_nn
 
-If you're enjoying using me, and/or would like to help me survive in the wild, hit /donate to help fund/upgrade my VPS!
+@mrflyman teşşekkürler
 """
 
 HELP_STRINGS = """
